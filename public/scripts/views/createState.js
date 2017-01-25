be1f1cbf1fd2f2195fb06f3fe2f645f270248887
juanchaves <chavesjuanm@gmail.com>
2017-01-25 14:31:48 -0800

    commit to rebase master in branch

--- public/scripts/views/createState.js
+++ public/scripts/views/createState.js
@@ -7,33 +7,4 @@ function clickStateInfo(e) {
 
   let clickedState = Data.all.filter((data) => data.state === e.target );
   console.log(clickedState);
-  }
-
-map.addListener('dblclick', clickStateInfo(e));
-
-
-geocodeLatLng(geocoder, map, infowindow);
-
-
-function geocodeLatLng(geocoder, map, infowindow) {
-  var input = document.getElementById('latlng').value;
-  var latlngStr = input.split(',', 2);
-  var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-  geocoder.geocode({'location': latlng}, function(results, status) {
-    if (status === 'OK') {
-      if (results[6]) {
-        map.setZoom(11);
-        var marker = new google.maps.Marker({
-          position: latlng,
-          map: map
-        });
-        infowindow.setContent(results[6].formatted_address);
-        infowindow.open(map, marker);
-      } else {
-        window.alert('No results found');
-      }
-    } else {
-      window.alert('Geocoder failed due to: ' + status);
-    }
-  });
 }
