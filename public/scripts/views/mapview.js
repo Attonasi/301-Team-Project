7c1917f7307ca4c696c038294c453557ad5f839c
Shiv Unadkat <Shiv Unadkat>
2017-01-27 09:26:12 -0800

    HOLY DAMN THE CHART STUFF WORKS

--- public/scripts/views/mapview.js
+++ public/scripts/views/mapview.js
@@ -71,20 +71,127 @@
         short_name = target[target.length-3].short_name;
       }
       allStatesClicked.push(long_name);
-      $('.state-data').html(makeHTML(long_name));
+      // $('.state-data').html(makeHTML(long_name));
+      $('#map-chart').html(drawChart(long_name));
   });
 }
-  function makeHTML(state){
+//
+// var myLatLng1 = new google.maps.LatLng(-33.891044,151.275537);
+// var marker1 = new google.maps.Marker({
+//   position: myLatLng1,
+//   map: map
+// })
+//
+// google.maps.event.addListener(marker1, 'click', function() {
+//      var infowindow1 = new google.maps.InfoWindow();
+//      infowindow1.setContent('Hey there! this shit might work!!');
+//      infowindow1.open(map, marker1);
+//    });
 
-    let currentState = getCurrentStateArr(state);
+// function initMap() {
+//   var uluru = {lat: -25.363, lng: 131.044};
+//   var map = new google.maps.Map(document.getElementById('map-canvas'), {
+//     zoom: 4,
+//     center: uluru
+//   });
+//
+//   var contentString = 'fuck';
+//
+//   var infowindow = new google.maps.InfoWindow({
+//     content: contentString
+//   });
+//
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//     title: 'Uluru (Ayers Rock)'
+//   });
+//   marker.addListener('click', function() {
+//     infowindow.open(map, marker);
+//   });
+// }
 
-    return `<h1>${state}</h1>
-    <h3>Heart Disease ${currentState[0]['Observed Deaths']}</h3>
-    <h3>Cancer ${currentState[1]['Observed Deaths']}</h3>
-    <h3>Stroke ${currentState[2]['Observed Deaths']}</h3>
-    <h3>Respiratory ${currentState[3]['Observed Deaths']}</h3>
-    <h3>Accidents ${currentState[4]['Observed Deaths']}</h3>`
+  // function makeHTML(state){
+  //   let currentState = getCurrentStateArr(state);
 
+    // var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    //   zoom: 4,
+    //   center: seattle
+    // });
+    //
+    // map.addListener('click', function() {
+    //   console.log('The map is alive');
+    // })
+    //
+    // var ctx = document.getElementById('map-chart').getContext('2d');
+    //
+    // function drawChart() {
+    //
+    //   var mapchart = new Chart(ctx, {
+    //     type: 'line',
+    //     data: {
+    //       labels: ['Heart Data', 'Cancer Data', 'Stroke Data', 'Chronic Respitory Data', 'Unintentional Injury Data'],
+    //       datasets: [{
+    //         label: 'Heart Data',
+    //         data: heartData,
+    //         backgroundColor: "rgba(153,255,51,0.4)"
+          // }, {
+          //   label: 'Cancer Data',
+          //   data: cancerData,
+          //   backgroundColor: "rgba(255,153,0,0.4)"
+          // }, {
+          //   label: 'Stroke Data',
+          //   data: strokeData,
+          //   backgroundColor: "rgba(255,153,0,0.4)"
+          // }, {
+          //   label: 'Chronic Respitory Data',
+          //   data: respData,
+          //   backgroundColor: "rgba(255,153,0,0.4)"
+          // }, {
+          //   label: 'Unintentional Injury Data',
+          //   data: uninData,
+          //   backgroundColor: "rgba(255,153,0,0.4)"
+//     return `<h1>${state}</h1>
+//     <h3>Heart Disease ${heartData}</h3>
+//     <h3>Cancer ${cancerData}</h3>
+//     <h3>Stroke ${strokeData}</h3>
+//     <h3>Respiratory ${respData}</h3>
+//     <h3>Accidents ${uninData}</h3>`
+// }
+
+function drawChart(state) {
+  let currentState = getCurrentStateArr(state);
+  var ctx = document.getElementById('map-chart').getContext('2d');
+  var mapchart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+    datasets: [{
+      label: 'Heart Data',
+      data: heartData,
+      backgroundColor: "rgba(153,255,51,0.4)"
+    }, {
+      label: 'Cancer Data',
+      data: cancerData,
+      backgroundColor: "rgba(255,153,0,0.4)"
+    }, {
+      label: 'Stroke Data',
+      data: strokeData,
+      backgroundColor: "rgba(255,153,0,0.4)"
+    }, {
+      label: 'Chronic Respitory Data',
+      data: respData,
+      backgroundColor: "rgba(255,153,0,0.4)"
+    }, {
+      label: 'Unintentional Injury Data',
+      data: uninData,
+      backgroundColor: "rgba(255,153,0,0.4)"
+    }]
+  },
+  options: {
+    legend: {labels:{fontColor:"black", fontSize: 14}}
+  }
+});
 }
 
 //
--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const app = express();
-const conString = process.env.DATABASE_URL || 'postgres://postgres:Joy1is1me!@localhost:5432/postgres';
+// const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
 
 // const conString = 'postgres://localhost:5432';
-const client = new pg.Client(conString);
-client.connect();
+// const client = new pg.Client(conString);
+// client.connect();
 
   // Github Proxy Function
 var proxyGitHub = function(request, response) {
